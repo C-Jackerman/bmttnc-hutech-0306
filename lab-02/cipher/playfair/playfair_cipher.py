fd298bed78eacf9bbb7d7e3d9eeb5ba47948bc13
C-Jackerman <ngocchien0751212@gmail.com>
2025-03-18 09:53:43 +0700

    lab-02 web

--- lab-02/cipher/playfair/playfair_cipher.py
+++ lab-02/cipher/playfair/playfair_cipher.py
@@ -1,75 +1,62 @@
-import numpy as np
-
-class PlayfairCipher:
-    def __init__(self, key):
-        self.key = key
-        self.matrix = self._generate_matrix(key)
-
-    def _generate_matrix(self, key):
-        key = key.upper().replace("J", "I")
-        key_set = []
-        for char in key:
-            if char not in key_set and char.isalpha():
-                key_set.append(char)
+class PlayFairCipher:
+    def __init__(self):
+        pass
+    
+    def create_playfair_matrix(self, key):
+        key = key.replace("J","I")
+        key = key.upper()
+        key_set = set(key)
+        alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+        remaining_letters = (letter for letter in alphabet if letter not in key_set)
+        matrix = list(key)
+         
+        for letter in remaining_letters:
+            matrix.append(letter)
+            if len(matrix) == 25:
+                break
         
-        for char in "ABCDEFGHIKLMNOPQRSTUVWXYZ":
-            if char not in key_set:
-                key_set.append(char)
+        playfair_matrix = [matrix[i:i+5]for i in range(0, len(matrix), 5)]
+        return playfair_matrix
+    
+    def find_letter_coords(self, matrix, letter):
+        for row in range(len (matrix)):
+            for col in range (len(matrix[row])):
+                if matrix[row][col] == letter:
+                    return row, col
         
-        return np.array(key_set).reshape(5, 5)
-
-    def _find_position(self, letter):
-        row, col = np.where(self.matrix == letter)
-        return row[0], col[0]
-
-    def encrypt(self, text):
-        text = text.upper().replace("J", "I").replace(" ", "")
-        pairs = []
-        i = 0
-        while i < len(text):
-            a = text[i]
-            b = text[i+1] if i + 1 < len(text) else 'X'
-            if a == b:
-                pairs.append((a, 'X'))
-                i += 1
-            else:
-                pairs.append((a, b))
-                i += 2
-        
+    def playfair_encrypt(self, plain_text, matrix):
+        plain_text = plain_text.replace("J","I")#chuyen J tthanh I trong van ban dau vao
+        plain_text = plain_text.upper()
         encrypted_text = ""
-        for a, b in pairs:
-            row1, col1 = self._find_position(a)
-            row2, col2 = self._find_position(b)
-            
-            if row1 == row2:
-                encrypted_text += self.matrix[row1, (col1 + 1) % 5]
-                encrypted_text += self.matrix[row2, (col2 + 1) % 5]
-            elif col1 == col2:
-                encrypted_text += self.matrix[(row1 + 1) % 5, col1]
-                encrypted_text += self.matrix[(row2 + 1) % 5, col2]
-            else:
-                encrypted_text += self.matrix[row1, col2]
-                encrypted_text += self.matrix[row2, col1]
         
-        return encrypted_text
-
-    def decrypt(self, text):
-        text = text.upper().replace(" ", "")
+        for i in range(0, len(plain_text), 2):
+            pair = plain_text[i:i+2]
+            if len(pair) == 1: #xu ly neu so luong ky tu le
+                pair += "X"
+            row1, col1 = self.find_letter_coords(matrix, pair[0])
+            row2, col2 = self.find_letter_coords(matrix, pair[1])
+                
+            if row1 == row2:
+                encrypted_text += matrix[row1][(col1+1)%5] + matrix[row2][(col2 + 1)%5]
+            elif col1 == col2:
+                encrypted_text += matrix[(row1+1)%5][col1] + matrix[(row2+1)%5][col2 ]
+            else:
+                encrypted_text += matrix[row1][col2] + matrix[row2][col1]
+        return encrypted_text   
+        
+    def playfair_decrypt(self, cipher_text, matrix):
+        cipher_text = cipher_text.upper()
         decrypted_text = ""
-        
-        for i in range(0, len(text), 2):
-            a, b = text[i], text[i+1]
-            row1, col1 = self._find_position(a)
-            row2, col2 = self._find_position(b)
+        for i in range(0, len(cipher_text), 2):
+            pair = cipher_text[i:i+2]
             
+            row1, col1 = self.find_letter_coords(matrix, pair[0])
+            row2, col2 = self.find_letter_coords(matrix, pair[1])
             if row1 == row2:
-                decrypted_text += self.matrix[row1, (col1 - 1) % 5]
-                decrypted_text += self.matrix[row2, (col2 - 1) % 5]
+                decrypted_text += matrix[row1][(col1 - 1)%5] + matrix[row2][(col2 - 1)%5]
             elif col1 == col2:
-                decrypted_text += self.matrix[(row1 - 1) % 5, col1]
-                decrypted_text += self.matrix[(row2 - 1) % 5, col2]
+                decrypted_text += matrix[(row1 - 1)%5][col1] + matrix[(row2-1)%5][col2]
             else:
-                decrypted_text += self.matrix[row1, col2]
-                decrypted_text += self.matrix[row2, col1]
-        
+                decrypted_text += matrix[row1][col2] + matrix[row2][col1]
         return decrypted_text
+            
--- lab-02/cipher/railfence/railfence_cipher.py
+++ lab-02/cipher/railfence/railfence_cipher.py
@@ -1,58 +1,50 @@
 class RailFenceCipher:
-    def __init__(self, rails: int):
-        self.rails = rails
 
-    def encrypt_text(self, text: str) -> str:
-        if self.rails <= 1:
-            return text
+    def __init__(self):
+        pass
 
-        rail_matrix = [''] * self.rails
+    def rail_fence_encrypt (self, plain_text, num_rails):
+        rails = [[] for _ in range (num_rails)]
         rail_index = 0
-        step = 1
+        direction = 1 
+        for char in plain_text:
+            rails[rail_index].append (char)
+            if rail_index == 0:
+                direction = 1
+            elif rail_index == num_rails-1:
+                direction = -1
+            rail_index += direction
+        cipher_text = ''.join(''.join(rail) for rail in rails)
+        return cipher_text
 
-        for char in text:
-            rail_matrix[rail_index] += char
-            rail_index += step
-
-            if rail_index == 0 or rail_index == self.rails - 1:
-                step *= -1  # Đảo chiều di chuyển
-
-        return ''.join(rail_matrix)
-
-    def decrypt_text(self, cipher_text: str) -> str:
-        if self.rails <= 1:
-            return cipher_text
-
-        rail_lengths = [0] * self.rails
-        rail_index = 0
-        step = 1
-
-        # Xác định độ dài của từng đường ray
-        for _ in cipher_text:
-            rail_lengths[rail_index] += 1
-            rail_index += step
-
-            if rail_index == 0 or rail_index == self.rails - 1:
-                step *= -1  # Đảo chiều di chuyển
-
-        rail_chars = []
-        index = 0
-
-        # Cắt từng đoạn của ciphertext vào rail_chars
+    def rail_fence_decrypt (self, cipher_text, num_rails):
+        rail_lengths=[0]* num_rails
+        rail_index=0
+        direction=1
+         
+        for _ in range (len (cipher_text)):
+            rail_lengths [rail_index] += 1  
+            if rail_index == 0:
+                direction = 1
+            elif rail_index==num_rails -1:
+                direction = -1
+            rail_index += direction
+        rails = []
+        start = 0
         for length in rail_lengths:
-            rail_chars.append(list(cipher_text[index:index + length]))
-            index += length
-
-        # Giải mã từ đường ray
-        rail_index = 0
-        step = 1
-        decrypted_text = []
-
-        for _ in cipher_text:
-            decrypted_text.append(rail_chars[rail_index].pop(0))
-            rail_index += step
-
-            if rail_index == 0 or rail_index == self.rails - 1:
-                step *= -1  # Đảo chiều di chuyển
-
-        return ''.join(decrypted_text)
+            rails.append(cipher_text [start:start + length])
+            start += length
+        
+        plain_text =""
+        rail_index=0
+        direction = 1
+        for _ in range (len (cipher_text)):
+            plain_text += rails[rail_index] [0]
+            rails[rail_index] = rails[rail_index] [1:]
+            if rail_index == 0:
+                direction = 1
+            elif rail_index ==num_rails - 1:
+                direction = -1
+            rail_index += direction
+            
+        return plain_text
